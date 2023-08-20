3aa283d960e1c145714f24d4b7c45375d91e06ec
Shivakirang <129768640+Shivakirang@users.noreply.github.com>
2023-08-20 21:29:06 +0530

    git hub test Update App.java

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "first new commit..");
+        System.out.println( "github test commit..");
     }
 }
